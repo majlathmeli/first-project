67459d7f1de5eb8c3c6be05f3c63b989f6faf247
Majlath Melinda <majlathmeli@gmail.com>
2019-11-06 20:24:11 +0100

    Első commit-om ever.

--- docs/teszt.py
+++ docs/teszt.py
@@ -6,9 +6,15 @@ driver.get ("https://majlathmeli.github.io/first-project/")
 driver.find_element (By.NAME, "a") .send_keys ("23")
 driver.find_element (By.NAME, "b") .send_keys ("23")
 
-header_text = driver.find_element (By.XPATH, "//h1").text
+header_text = driver.find_element (By.XPATH, "//h1").get_attribute ("value")
 print (header_text)
 assert header_text == "Számológép"
 
 driver.find_element (By.XPATH, "//input[3]").click()
+
+result = driver.find_element (By.ID, "result-input").click()
+
+drive.save_screenshot ("result.png")
+
+assert result == "46"
 
